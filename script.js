3cee808a771493b56b2f74c3c4c6654599a32d32
Rudrian <162451660+Rudrian@users.noreply.github.com>
2025-03-18 15:22:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Arquivo de script vazio 
+//Atv1
+
+function azul(){
+    const azul = document.createElement('div');
+    azul.classList.add('azul');
+    document.body.appendChild(azul);
+}
+
+function vermelho(){
+    const vermelho = document.createElement('div');
+    vermelho.classList.add('vermelho');
+    document.body.appendChild(vermelho);
+}
+
+//Atv2
